b7eacb7d925cfce8b1f42938e39425ab0b4c13f0
LoganRiddle <121885823+LoganRiddle@users.noreply.github.com>
2023-05-04 10:23:56 -0400

    Version 0.0.3: Bug fixes

--- src/inferno.c
+++ src/inferno.c
@@ -4,10 +4,89 @@
 #include <time.h>
 #include <unistd.h>
 
-//#define SZ 7500
+#define SZ 7500
 
-#include "score.c"
-#include "tests.c"
+// Global Variables
+float A[SZ][SZ];
+
+
+int test(int length){	
+	time_t start, current;
+	time(&start);
+
+	int temp;
+ 	size_t temp_size = sizeof(temp);
+
+	int i, j, count;
+	float wps;
+	double elapsed;
+	FILE* test_file = fopen(".inferno_test.txt", "w");
+	char str[100];
+	int test_length = 0;
+
+	if (test_file == NULL) {
+    		printf("Error: unable to open file test.txt\n");
+    		exit(EXIT_FAILURE);
+  	}
+
+	if(length == 1){
+		test_length = SZ;
+	}else if(length == 2){
+		test_length = SZ/2;
+	}else if(length == 4){
+		test_length = SZ/4;
+	}else{
+		exit(EXIT_FAILURE);
+	}
+
+	for(i = 0; i < test_length; i++) {
+       		for(j = 0; j < test_length; j++) {
+          		A[i][j] = i*j;
+       		}
+	}
+
+	for(i = 0; i < test_length; i++) {
+       		for(j = 1; j < test_length; j++) {
+          		A[i][j] = ( A[i][j-1] + A[i][j] ) / 2;
+  			time(&current);
+
+    			// Calculate the elapsed time in seconds
+   			elapsed = difftime(current, start);
+			
+			wps = count/elapsed;
+
+    			// Print the elapsed time
+    			printf("Elapsed time: %.0f seconds. Write speed: %.02f writes per second\r", elapsed, wps);
+			fflush(stdout);
+
+			sprintf(str, "%f", A[i][j]);
+			fprintf(test_file, str);
+			count += 1;
+       		}
+    	}
+
+	double awps = count/elapsed;
+	printf("\nAverage Write Speed: %.02f writes per second\n", awps);
+	printf("Short Test Ended Successfully\n");
+
+	return 0;
+}
+
+
+int cpu_info(void){
+	FILE *cpuinfo = fopen("/proc/cpuinfo", "rb");
+	char *arg = 0;
+   	size_t size = 0;
+   
+	while(getdelim(&arg, &size, 0, cpuinfo) != -1){
+      		puts(arg);
+   	}
+   	
+	free(arg);
+   	fclose(cpuinfo);
+  	
+	return 0;
+}
 
 
 int cleanup(void){
@@ -22,9 +101,12 @@ int cleanup(void){
 
 int main(void){
 	char userin[100];
-	system("clear");
-	//system("neofetch");
+	int length = 0;
 
+	system("clear");
+	//system("lscpu");
+	//system("cat /proc/cpuinfo | grep processor | wc -l");	
+	
 	printf("Welcome to Inferno\n");
 	printf("\nChoose which test to run:\n");
 	printf("=========================\n");
@@ -34,11 +116,14 @@ int main(void){
 	printf("\n");
 
 	if(strcmp(userin, "short") == 0 || strcmp(userin, "a") == 0 || strcmp(userin, "A") == 0){
-		short_test();
+		length = 4;
+		test(length);
 	}else if(strcmp(userin, "normal") == 0 || strcmp(userin, "b") == 0 || strcmp(userin, "B") == 0){
-		normal_test();
+		length = 2;
+		test(length);
 	}else if(strcmp(userin, "extended") == 0 || strcmp(userin, "c") == 0 || strcmp(userin, "C") == 0){
-		extended_test();
+		length = 1;
+		test(length);
 	}else{
 		printf("Error: userinput not matching options.");
 		exit(EXIT_FAILURE);
--- src/score.c
+++ src/score.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 
-int score(void){
-    
-    
-    return 0;
+int score(void){    
+		
+
+	return 0;
 }
