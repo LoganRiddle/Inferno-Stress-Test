866eb678bed1618290a386615cd61c5ecbf42471
LoganRiddle <121885823+LoganRiddle@users.noreply.github.com>
2023-05-25 09:41:54 -0400

    Test

--- src/ui.c
+++ src/ui.c
@@ -6,8 +6,8 @@ int ui(void){
 	system("clear");
 
 	// Builds terminal interface with 	
-	printf("Welcome to Inferno\n");
-
+	printf("Welcome to Inferno\n==================\n\n");
+	system("grep -m 1 'model name' /proc/cpuinfo");
 	printf("\nChoose which test to run:\n");
 	printf("=========================\n");
 	printf("a.) (short) \nb.) (normal) \nc.) (extended)\nq.) (quit)\n\n ");
